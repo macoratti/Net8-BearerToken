863c526531f751278cd8a17e81fde58bd9056884
Macoratti <Macoratti@DESKTOP-DK57UNP>
2023-11-14 12:49:58 -0300

    Add project files.

---
+++ Net8BearerToken/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Net8BearerToken.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+
+    [HttpGet("/login")]
+    public IActionResult Login(string username, string password)
+    {
+
+        if (IsValidUser(username, password))
+        {
+            var claimsPrincipal = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new[] { new Claim(ClaimTypes.Name, username) },
+                    BearerTokenDefaults.AuthenticationScheme
+                )
+            );
+
+            return SignIn(claimsPrincipal);
+        }
+        return Unauthorized("Credenciais inválidas");
+    }
+
+    private bool IsValidUser(string username, string password)
+    {
+        return username == "macoratti" && password == "123456";
+    }
+
+    [HttpGet("/user")]
+    [Authorize]
+    public IActionResult GetUser()
+    {
+        var user = User;
+        if (user?.Identity?.IsAuthenticated ?? false)
+        {
+            return Ok($"Bem-Vindo {user.Identity.Name}!");
+        }
+
+        return Unauthorized();
+    }
+}
---
+++ Net8BearerToken/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "apinet8", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "Token",
+        In = ParameterLocation.Header,
+        Description = "Token Access",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+});
+
+
+
+builder.Services.AddAuthentication().AddBearerToken();
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
